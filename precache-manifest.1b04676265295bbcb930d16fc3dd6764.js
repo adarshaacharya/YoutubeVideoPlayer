5b4e2f795a26e287007fc5df82430ebf1d498e46
Aadarsha <connectwithadarsha@gmail.com>
2020-03-30 21:51:05 +0545

    Updates

---
+++ precache-manifest.1b04676265295bbcb930d16fc3dd6764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fcf1d414dd5b43ff70969da67a2a6c3",
+    "url": "/YoutubeVideoPlayer/index.html"
+  },
+  {
+    "revision": "4e495bdab05e8e06f7bd",
+    "url": "/YoutubeVideoPlayer/static/css/main.cdd8167b.chunk.css"
+  },
+  {
+    "revision": "d7be8fc551fa0504e800",
+    "url": "/YoutubeVideoPlayer/static/js/2.e4fabc7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YoutubeVideoPlayer/static/js/2.e4fabc7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e495bdab05e8e06f7bd",
+    "url": "/YoutubeVideoPlayer/static/js/main.eac4c0a2.chunk.js"
+  },
+  {
+    "revision": "a0f72545201b889445cb",
+    "url": "/YoutubeVideoPlayer/static/js/runtime-main.3f72874a.js"
+  }
+]);
